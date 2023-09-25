517e2c37b3bae7417ec5c03b9efbd5eadacc331e
themhz <themhz@gmail.com>
2023-09-25 18:34:20 +0300

    saving code file, open code file

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,6 +32,10 @@
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.splitContainer2 = new System.Windows.Forms.SplitContainer();
             this.scintilla1 = new ScintillaNET.Scintilla();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuSave = new System.Windows.Forms.ToolStripMenuItem();
             this.label1 = new System.Windows.Forms.Label();
             this.txtUrl = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
@@ -45,6 +49,7 @@
             this.splitContainer2.Panel1.SuspendLayout();
             this.splitContainer2.Panel2.SuspendLayout();
             this.splitContainer2.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // webView21
@@ -86,6 +91,7 @@
             // splitContainer2.Panel1
             // 
             this.splitContainer2.Panel1.Controls.Add(this.scintilla1);
+            this.splitContainer2.Panel1.Controls.Add(this.menuStrip1);
             // 
             // splitContainer2.Panel2
             // 
@@ -100,15 +106,52 @@
             // scintilla1
             // 
             this.scintilla1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.scintilla1.Location = new System.Drawing.Point(0, 0);
+            this.scintilla1.Location = new System.Drawing.Point(0, 30);
             this.scintilla1.Name = "scintilla1";
-            this.scintilla1.Size = new System.Drawing.Size(517, 426);
+            this.scintilla1.Size = new System.Drawing.Size(517, 396);
             this.scintilla1.TabIndex = 2;
+            this.scintilla1.TextChanged += new System.EventHandler(this.scintilla1_TextChanged);
+            this.scintilla1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.scintilla1_KeyDown);
+            this.scintilla1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.scintilla1_KeyPress);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(517, 30);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuOpen,
+            this.menuSave});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(46, 26);
+            this.toolStripMenuItem1.Text = "File";
+            // 
+            // menuOpen
+            // 
+            this.menuOpen.Name = "menuOpen";
+            this.menuOpen.Size = new System.Drawing.Size(224, 26);
+            this.menuOpen.Text = "Open";
+            this.menuOpen.Click += new System.EventHandler(this.menuOpen_Click);
+            // 
+            // menuSave
+            // 
+            this.menuSave.Name = "menuSave";
+            this.menuSave.Size = new System.Drawing.Size(224, 26);
+            this.menuSave.Text = "Save";
+            this.menuSave.Click += new System.EventHandler(this.menuSave_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 27);
+            this.label1.Location = new System.Drawing.Point(8, 79);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(24, 16);
             this.label1.TabIndex = 3;
@@ -116,7 +159,7 @@
             // 
             // txtUrl
             // 
-            this.txtUrl.Location = new System.Drawing.Point(39, 27);
+            this.txtUrl.Location = new System.Drawing.Point(38, 79);
             this.txtUrl.Name = "txtUrl";
             this.txtUrl.Size = new System.Drawing.Size(379, 22);
             this.txtUrl.TabIndex = 2;
@@ -124,7 +167,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(424, 27);
+            this.button2.Location = new System.Drawing.Point(423, 79);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(79, 23);
             this.button2.TabIndex = 1;
@@ -134,7 +177,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(39, 72);
+            this.button1.Location = new System.Drawing.Point(38, 124);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(147, 23);
             this.button1.TabIndex = 0;
@@ -156,10 +199,13 @@
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
             this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel1.PerformLayout();
             this.splitContainer2.Panel2.ResumeLayout(false);
             this.splitContainer2.Panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
             this.splitContainer2.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -174,6 +220,10 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtUrl;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem menuSave;
+        private System.Windows.Forms.ToolStripMenuItem menuOpen;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -8,6 +8,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Reflection.Emit;
 using System.Runtime.InteropServices;
@@ -19,9 +20,13 @@ namespace WebViewJsDebugger
 {
     public partial class Form1 : Form
     {
+        private string currentFilePath = null;
+
         public Form1()
         {
+                        
             InitializeComponent();
+            this.KeyPreview = true;
             scintilla1.Lexer = Lexer.Cpp;
 
             scintilla1.Styles[Style.Cpp.Default].ForeColor = Color.Silver;
@@ -96,5 +101,107 @@ namespace WebViewJsDebugger
         {
             Debug.WriteLine("BytesReceivedChanged " +downloadOperation.BytesReceived.ToString()); // Show the progress
         }
+
+        private void menuOpen_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 1;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    currentFilePath = openFileDialog.FileName;
+
+                    try
+                    {
+                        scintilla1.Text = File.ReadAllText(currentFilePath);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Error reading file: {ex.Message}");
+                    }
+                }
+            }
+        }
+
+        private void menuSave_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(currentFilePath))
+            {
+                using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+                {
+                    saveFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+                    saveFileDialog.FilterIndex = 1;
+                    saveFileDialog.RestoreDirectory = true;
+
+                    if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        currentFilePath = saveFileDialog.FileName;
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+            }
+
+            try
+            {
+                File.WriteAllText(currentFilePath, scintilla1.Text);
+                MessageBox.Show("File saved successfully!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error saving file: {ex.Message}");
+            }
+        }
+
+        private void scintilla1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.S)
+            {
+                menuSave_Click(this, EventArgs.Empty); // Call the save method
+                e.Handled = true;  // Indicate that you've handled this key event                
+                e.SuppressKeyPress = true;  // Prevent the key press from being sent to other controls
+            }
+        }
+
+        private void scintilla1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 19) // Ctrl + S is represented by the ASCII value 19
+            {
+                e.Handled = true; // Prevent further processing of the character
+            }
+        }
+
+        private void SetLineNumberMargin(Scintilla scintillaControl)
+        {
+            int maxLineNumberCharLength = scintillaControl.Lines.Count.ToString().Length;
+            const int padding = 2;  // Give a little padding
+            scintillaControl.Margins[0].Width = scintillaControl.TextWidth(Style.LineNumber, new string('9', maxLineNumberCharLength + 1)) + padding;
+        }
+
+        private void scintilla1_TextChanged(object sender, EventArgs e)
+        {
+            SetLineNumberMargin(sender as Scintilla);
+        }
+
+        private void SetupScintilla(Scintilla scintillaControl)
+        {
+            // Set the line numbers
+            scintillaControl.Styles[Style.LineNumber].BackColor = Color.LightGray;
+            scintillaControl.Styles[Style.LineNumber].ForeColor = Color.Black;
+            scintillaControl.Styles[Style.LineNumber].Font = "Consolas";
+            scintillaControl.Styles[Style.LineNumber].Size = 9;
+
+            scintillaControl.Margins[0].Type = MarginType.Number;
+            scintillaControl.Margins[0].Mask = 0;
+            scintillaControl.Margins[0].Sensitive = false;
+            scintillaControl.Margins[0].Width = 16;  // Initial width, will be adjusted by the SetLineNumberMargin method
+        }
+
     }
 }
